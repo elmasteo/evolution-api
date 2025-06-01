861638b4b135f3c83e33e11b871d1b13db1bc3c6
elmasteo <30936419+elmasteo@users.noreply.github.com>
2025-05-31 19:47:52 -0500

    Update .render-start.sh

--- .render-start.sh
+++ .render-start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Ejecutando migraciones con PostgreSQL"
+# Ejecutar migraciones de PostgreSQL
 npx prisma migrate deploy --schema=./prisma/postgresql-schema.prisma
 
-echo "Iniciando Evolution API"
+# Iniciar la API en el puerto que Render espera
+PORT=${PORT:-3000}
+echo "Iniciando en el puerto $PORT"
 npm run start
